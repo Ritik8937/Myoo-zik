4c8ce41f0f465a33a98b6ef274da05eaee09f40f
Ritik <ritiksharmars987@gmail.com>
2020-09-22 13:16:27 +0530

    Initial commit

---
+++ Myoozik/app/src/main/java/com/example/myoo_zik/MusicFiles.java
@@ -0,0 +1,4 @@
+package com.example.myoo_zik;
+
+public class MusicFiles {
+}
---
+++ Myoozik/app/src/main/java/com/example/myoo_zik/PlayerActivity.java
@@ -0,0 +1,13 @@
+package com.example.myoo_zik;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
